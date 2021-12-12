9b4a846ea3576033f7b2d056611a4ec4218c7e8f
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-12 23:56:26 +0400

    refactor: added new requests

--- script.js
+++ script.js
@@ -9,6 +9,31 @@ function getTodos(){
 
 document.getElementById('get').addEventListener('click',getTodos);
 
+function addTodo(){
+    axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title:"New Todo",
+        completed:false
+    })
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err));
+}
+
+//Put/Patch request------------------
+function updateTodo(){
+    axios.put('https://jsonplaceholder.typicode.com/todos/1',{
+        title:"Updated Todo",
+        completed:true
+    })
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err));
+}
+
+//Delete request--------------
+function removeTodo(){
+    axios.delete('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err));
+}
 
 
 
